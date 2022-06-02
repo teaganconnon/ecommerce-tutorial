aeb2a7712133596bd2ed3ec6d227524269773b1d
teaganconnon <teagan@ucsb.edu>
2022-06-02 11:06:42 -0700

    added scss stylesheets + header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import "./default.scss";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <h2>Hello World</h2>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.scss";
+
+const Header = (props) => {
+  return <header></header>;
+};
+
+export default Header;
