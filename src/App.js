3535e958afa79b9016ae51b58d2808ee98b44e8b
teaganconnon <teagan@ucsb.edu>
2022-06-03 12:05:47 -0700

    added react-router-dom to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import Header from "./components/Header";
 import Homepage from "./pages/Homepage";
+import Registration from "./pages/Registration";
 import "./default.scss";
 
 function App() {
@@ -8,7 +10,8 @@ function App() {
     <div className="App">
       <Header />
       <div className="main">
-        <Homepage />
+        <Route path="/" component={Homepage} />
+        <Route path="/registration" component={Registration} />
       </div>
     </div>
   );
